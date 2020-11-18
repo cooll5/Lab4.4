886824453ab92b4c3a1d1500e934ba159f89bc96
cooll5 <52448498+cooll5@users.noreply.github.com>
2020-11-18 22:42:01 +0200

    first commit

---
+++ Lab4.4/Lab4.4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, R, dx, y;
+	cout << "R = "; cin >> R;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= -8)
+			y = -R;
+		else
+			if (-R < x && x <= R)
+				y >= R - x;
+			else
+				if (R < x && x <= 2 * R)
+					y = R - x;
+				else
+					if (x >= 2 * R)
+						y = R;
+
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
